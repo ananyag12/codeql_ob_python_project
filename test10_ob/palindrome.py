e9d0406d978fb7c4eb1c94192248e04bcf05a84b
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-03 15:50:38 -0500

    Create palindrome.py

---
+++ test10_ob/palindrome.py
@@ -0,0 +1,11 @@
+def main ():#line:1:def main():
+    try :#line:2:try:
+        OO00OOOOO000OO0O0 =input ("Enter a string: ").lower ()#line:3:text = input("Enter a string: ").lower()
+        if OO00OOOOO000OO0O0 ==OO00OOOOO000OO0O0 [::-1 ]:#line:4:if text == text[::-1]:
+            print ("The string is a palindrome.")#line:5:print("The string is a palindrome.")
+        else :#line:6:else:
+            print ("The string is not a palindrome.")#line:7:print("The string is not a palindrome.")
+    except ValueError :#line:9:except ValueError:
+        print ("Invalid input! Please enter a string.")#line:10:print("Invalid input! Please enter a string.")
+if __name__ =="__main__":#line:12:if __name__ == "__main__":
+    main ()
