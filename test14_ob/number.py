a1ba4633f63d07c9e99054ce4249e0d9ce380936
Ananya Gollakota <76816318+ananyag12@users.noreply.github.com>
2024-12-05 22:57:20 -0500

    Create number.py

---
+++ test14_ob/number.py
@@ -0,0 +1,10 @@
+def main ():#line:1
+    OOO00O0OOO0O00OOO =float (input ("Enter a number: "))#line:2
+    if OOO00O0OOO0O00OOO >0 :#line:3
+        print ("Positive")#line:4
+    elif OOO00O0OOO0O00OOO <0 :#line:5
+        print ("Negative")#line:6
+    else :#line:7
+        print ("Zero")#line:8
+if __name__ =="__main__":#line:10
+    main ()
